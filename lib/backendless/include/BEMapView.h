21d857033bd9600734ec1a10fffe6e2aae3c0a91
Vyacheslav Vdovichenko <slavav@slavav.local>
2014-05-05 15:22:53 +0300

    refactoring: Reachability -> BEReachability class BKNDLSS-5214

--- lib/backendless/include/BEMapView.h
+++ lib/backendless/include/BEMapView.h
@@ -1,16 +1,29 @@
 //
 //  BEMapView.h
 //  backendlessAPI
-//
-//  Created by Yury Yaschenko on 10/30/13.
-//  Copyright (c) 2013 BACKENDLESS.COM. All rights reserved.
-//
+/*
+ * *********************************************************************************************************************
+ *
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *
+ *  ********************************************************************************************************************
+ *
+ *  Copyright 2014 BACKENDLESS.COM. All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *
+ *  ********************************************************************************************************************
+ */
 
 #import <MapKit/MapKit.h>
+
 @class BackendlessCollection, Fault, BackendlessGeoQuery, GeoPoint;
 
 @interface BEMapView : MKMapView
-
 @property (nonatomic, strong) NSDictionary *metadata;
 @property (nonatomic, copy) NSString *whereClause;
 
@@ -28,6 +41,10 @@
 //-(void)relativeFind:(BackendlessGeoQuery *)query responder:(id)responder;
 //-(void)getPoints:(BackendlessGeoQuery *)query response:(void(^)(BackendlessCollection *))responseBlock error:(void(^)(Fault *))errorBlock;
 //-(void)relativeFind:(BackendlessGeoQuery *)query response:(void(^)(BackendlessCollection *))responseBlock error:(void(^)(Fault *))errorBlock;
-
+-(void)setUnits:(int)units;
 -(void)removeAllObjects;
+-(NSArray *)responseData;
+
+-(void)setSearchWithRadius:(float)radius;
+-(void)setSearchInMapBoundaries;
 @end
